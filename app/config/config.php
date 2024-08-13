ed39468b99c55247c9515e199e915c04e30624f4
Ahozein <aho3ein_d79@yahoo.com>
2024-08-13 10:49:45 +0330

    PDO Database class

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,11 @@
 <?php
 
+//DB Parameters
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS','');
+define('DB_NAME', 'php_mvc');
+
 define('APP_ROOT', dirname(__DIR__));
 define('URL_ROOT', 'http://localhost/php_mvc/');
 define('SITE_NAME', 'PHP MVC');
---
+++ app/libraries/Database.php
@@ -0,0 +1,73 @@
+<?php
+/* PDO Database Class
+ * Connect to database
+ * Create Prepared statements
+ * Bind Values
+ * Return rows and value
+ */
+
+class Database
+{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $db_handler;
+    private $stmt;
+    private $error;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname . ';charset=utf8';
+
+        //Create PDO instance
+        try {
+            $this->db_handler = new PDO($dsn, $this->user, $this->pass);
+            $this->db_handler->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+    //Prepare Statement with query
+    public function query($sql)
+    {
+        $this->stmt = $this->db_handler->prepare($sql);
+    }
+
+    //Bind value
+    public function bind($param, $value, $type = null)
+    {
+        $this->stmt->bindParam($param, $value, $type);
+    }
+
+    //Execute the prepared statement
+    public function execute()
+    {
+        return $this->stmt->execute();
+    }
+    
+    //Get result set as array of object
+    public function fetchAll()
+    {
+        $this->execute();
+        return $this->stmt->fetchAll();
+    }
+
+    //Get single record as object
+    public function fetch()
+    {
+        $this->execute();
+        return $this->stmt->fetch();
+    }
+
+    //Get row count
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
+
+}
